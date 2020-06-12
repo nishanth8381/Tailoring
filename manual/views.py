fe9ec0b730375cb9b88bdc48671dee96013d5a28
Nishanth <nishanth.nuthakki@gmail.com>
2020-06-12 19:06:37 +0530

    started app

--- manual/views.py
+++ manual/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def List(request):
+    return render(request,"list.html")
